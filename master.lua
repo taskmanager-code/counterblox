915ab309db0cee29923ee0534ece231e37564b0c
taskmanager-code <69488507+taskmanager-code@users.noreply.github.com>
2021-08-15 11:12:46 -1000

    Update master.lua

--- master.lua
+++ master.lua
@@ -1,1 +1,11 @@
+viewmodel.on_new_viewmodel:Connect(function(module)
+    local arm_parts = module.get_arm_parts()
+    local weapon_parts = module.get_weapon_parts()
 
+    for _,weapon_part in pairs (weapon_parts) do
+        if weapon_part:IsA("MeshPart") then weapon_part.TextureID = "" end
+
+        weapon_part.Color = Color3.fromRGB(0, 255, 0)
+        weapon_part.Material = "ForceField"
+    end
+end)
